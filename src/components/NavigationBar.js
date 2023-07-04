84c8624e150e3a05af4708871a0c8138489bd418
Manikandan <manikandanprabacse@gmail.com>
2023-07-04 07:59:38 +0530

    Added NavigationBar with search option

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,87 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Button, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import SearchInput from "./search/SearchInput";
+
+function NavigationBar(props) {
+  const { isLogged, isAdmin } = props;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+  return (
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: "#3f51b5",
+      }}
+    >
+      <Toolbar>
+        <Grid container spacing={2}>
+          <Grid item xs={5}>
+            <IconButton
+              size="small"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <ShoppingCartIcon />
+              <Typography variant="body1" component="span">
+                upGrad E-Shop
+              </Typography>
+            </IconButton>
+          </Grid>
+          <Grid item xs={3}>
+            <SearchInput />
+          </Grid>
+          <Grid item xs={4} textAlign={"right"}>
+            {isLogged ? (
+              <div>
+                <Button
+                  color="inherit"
+                  variant="text"
+                  onClick={() => navigate(isLogged ? "/products" : "/login")}
+                >
+                  Home
+                </Button>
+                {isAdmin && (
+                  <Button
+                    color="inherit"
+                    variant="text"
+                    onClick={() => navigate("/add-product")}
+                  >
+                    Add Product
+                  </Button>
+                )}
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button color="inherit" variant="text">
+                  Login
+                </Button>
+                <Button color="inherit" variant="text">
+                  Sign Up
+                </Button>
+              </div>
+            )}
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavigationBar;
